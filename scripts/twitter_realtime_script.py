e2239a16fc95f14363582bb336912f87800f0f89
marckysan <e0425377@u.nus.edu>
2023-03-05 03:22:21 +0800

    Streaming twitter data into bq

--- scripts/twitter_realtime_script.py
+++ scripts/twitter_realtime_script.py
@@ -1,14 +1,11 @@
+import io
 import json
 import os
-import smtplib
-import ssl
-import sys
+from io import StringIO
 
 import requests
-import tweepy
+from google.api_core.exceptions import BadRequest
 from google.cloud import bigquery
-from tweepy import Cursor, OAuthHandler, Stream
-from tweepy.streaming import Stream
 
 ########################## ALL CREDENTIALS - REDDIT & BQ ##############################
 
@@ -107,9 +104,48 @@ def set_rules(delete, rules):
     print(json.dumps(response.json()))
 
 
+def insert_data_into_BQ(data_as_file) :
+    schema = [
+        bigquery.SchemaField("author_id", "INT64", mode="NULLABLE"),
+        bigquery.SchemaField("created_at", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("id", "INT64", mode="NULLABLE"),
+        bigquery.SchemaField("text", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField(
+            "matching_rules",
+            "RECORD",
+            mode = "REPEATED",
+            fields=[
+                bigquery.SchemaField("id", "INT64", mode="NULLABLE"),
+                bigquery.SchemaField("tag", "STRING", mode="NULLABLE"),
+            ],
+        )
+    ]
+
+    client = bigquery.Client()
+    table_id = "crypto3107.twitter.realtime_tweets"
+    job_config = bigquery.LoadJobConfig(
+        schema=schema,
+        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+    )
+    load_job = client.load_table_from_file(
+        data_as_file,
+        table_id,
+        location="asia-southeast1",
+        job_config=job_config,
+    ) 
+
+    try:
+        load_job.result()
+        print("Successfully uploaded data to BigQuery!")
+    except BadRequest:
+        for error in load_job.errors:
+            print(error["message"])
+    
+    destination_table = client.get_table(table_id)
+
 def get_stream(set):
     response = requests.get(
-        "https://api.twitter.com/2/tweets/search/stream", auth=bearer_oauth, stream=True,
+        "https://api.twitter.com/2/tweets/search/stream?tweet.fields=author_id,created_at,text", auth=bearer_oauth, stream=True,
     )
     print(response.status_code)
     if response.status_code != 200:
@@ -121,9 +157,28 @@ def get_stream(set):
     for response_line in response.iter_lines():
         if response_line:
             json_response = json.loads(response_line)
-            print(json.dumps(json_response, indent=4, sort_keys=True))
-
-
+            data = json_response.get("data")
+            matching_rules = json_response.get("matching_rules")
+            
+            # Setting structure of data to be inserted into BQ
+            structured_response = [{
+                'author_id': data.get('author_id'),
+                'created_at': data.get('created_at'),
+                'id': data.get('id'),
+                'text': data.get('text'),
+                'matching_rules': matching_rules
+            }]
+            
+            # Formatting and manipulating data to a format that can be inserted into BQ
+            streaming_json = StringIO(json.dumps(structured_response, indent=4, sort_keys=True))        
+            streaming_result = [json.dumps(record) for record in json.load(streaming_json)] 
+            streaming_formatted_json = '\n'.join(streaming_result)
+            data_as_file = io.StringIO(streaming_formatted_json) # Put ndjson into file 
+            
+            # Insert into BQ
+            insert_data_into_BQ(data_as_file)
+            print(f"SUCCESSFULLY INSERTED TWITTER STREAMING RECORD ID {data.get('id')} INTO BQ")
+            
 def extract_twitter_realtime_coin_news_data():
     rules = get_rules()
     delete = delete_all_rules(rules)
