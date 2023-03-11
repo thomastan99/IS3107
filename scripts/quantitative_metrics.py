01a98954f6690edd0dc64b7d882a0c1943b83698
thomastan99 <71714178+thomastan99@users.noreply.github.com>
2023-03-11 16:02:46 +0800

    made quantitative metrics into a function

--- scripts/quantitative_metrics.py
+++ scripts/quantitative_metrics.py
@@ -2,24 +2,27 @@ from google.cloud import bigquery
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./creds/cred.json"
 client = bigquery.Client()
+def pull_coin_data(coin_name):
 
-query = """
-with deduped_table as (
-  select distinct * from `crypto3107.coincap.bitcoin`
-)
-SELECT 
-DATE(PARSE_timestamp('%Y-%m-%dT%H:%M:%E*S%Ez', date)) AS date,
-  priceUsd,
-  CASE 
-    WHEN priceUsd > LAG(priceUsd) OVER (ORDER BY date) THEN 1 
-    ELSE 0 
-  END AS is_greater
-from deduped_table
+    query = f"""
+    with deduped_table as (
+    select distinct * from `crypto3107.coincap.{coin_name}`
+    )
+    SELECT 
+    DATE(PARSE_timestamp('%Y-%m-%dT%H:%M:%E*S%Ez', date)) AS date,
+    priceUsd,
+    CASE 
+        WHEN priceUsd > LAG(priceUsd) OVER (ORDER BY date) THEN 1 
+        ELSE 0 
+    END AS is_greater
+    from deduped_table
 
-order by date desc
+    order by date desc
 
-"""
+    """
 
-results = client.query(query).to_dataframe()
+    results = client.query(query).to_dataframe()
 
-print(results)
+    print(results)
+
+pull_coin_data("bitcoin")
